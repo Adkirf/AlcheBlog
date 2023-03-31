a67707df25a1f71267d429cc42638df7817a723a
Adkirf <adkirfuglyape@gmail.com>
2023-03-31 19:54:11 +0200

    delete client

--- client/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-const removeImports = require('next-remove-imports')();
-module.exports = removeImports({});
-
-module.exports = nextConfig
